50d6be8a94003f83bb337be364e6d5a2a075e81f
chrisoney <christopher.oney@yahoo.com>
2021-07-14 09:57:00 -0700

    toggle user dropdown

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -6,9 +6,16 @@ buttons.forEach((button) => button.addEventListener('click', async (e) => {
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({stage})
+    body: JSON.stringify({ stage })
   })
   if (res.ok) {
     window.location.reload();
   }
-}))
+}));
+
+const user_menu_toggle_button = document.querySelector('.user-dropdown-button');
+const user_menu = document.querySelector('.user-options');
+
+user_menu_toggle_button.addEventListener('click', (e) => {
+  user_menu.classList.toggle('hidden')
+})
