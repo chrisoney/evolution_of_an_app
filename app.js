17379ee0641197c8bf55a03c85fe3577cf965bed
chrisoney <christopher.oney@yahoo.com>
2021-07-16 10:10:21 -0700

    add edit button for new page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const bookshelvesRouter = require('./routes/bookshelves');
 const apiRouter = require('./routes/api')
 
 const { restoreUser } = require('./auth')
@@ -45,6 +46,7 @@ store.sync();
 app.use(restoreUser)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/bookshelves', bookshelvesRouter);
 app.use('/api', apiRouter)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/bookshelves.js
+++ routes/bookshelves.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { Bookshelf } = require('../db/models');
+const { asyncHandler } = require('./utils');
 
 router.get('/edit', asyncHandler(async (req, res) => {
-  const bookshelves = await Bookshelf.findAll()
+  const userId = req.session.auth.userId;
+  const bookshelves = await Bookshelf.findAll({ where: { userId } })
+  
+  res.render('bookshelf-edit', { bookshelves })
 }))
 
 module.exports = router;
