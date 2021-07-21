0ea09abe21dbe90eebad5ba2e0dde70c5f34beab
chrisoney <christopher.oney@yahoo.com>
2021-07-20 17:16:00 -0700

    story page bookshelf button styled

--- routes/stories.js
+++ routes/stories.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const { asyncHandler, pageChecker } = require('./utils');
-const { Story } = require('../db/models')
+const { Story, Bookshelf } = require('../db/models')
 const router = express.Router();
 
 router.use(pageChecker)
 
 router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
   const id = req.params.id;
-  const story = await Story.findByPk(id);
-
-  res.render('story-page', { story })
+  let wantToReadId;
+  const bookshelves = await Bookshelf.findAll({
+    where: { userId: req.session.auth.userId }
+  })
+  const story = await Story.findByPk(id, {
+    include: {
+      model: Bookshelf,
+      where: { userId: req.session.auth.userId },
+      required: false
+    }
+  });
+  for (let i = 0; i < bookshelves.length; i++){
+    if (bookshelves[i].name === 'Want To Read') wantToReadId = bookshelves[i].id;
+  }
+  res.render('story-page', { story, bookshelves, wantToReadId })
   // res.json({ story })
 }))
 
